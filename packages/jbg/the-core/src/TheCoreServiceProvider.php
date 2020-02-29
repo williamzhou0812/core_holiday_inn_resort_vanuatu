92f39800c0db67b89375ed711d395eb9765334a9
Chee <tobyl19@hotmail.com>
2020-02-29 13:59:55 +0900

    Add package dummy

---
+++ packages/jbg/the-core/src/TheCoreServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace JBG\TheCore;
+
+use Illuminate\Support\ServiceProvider as SupportServiceProvider;
+/*use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Str;
+use Intervention\Image\ImageServiceProvider;
+use Larapack\DoctrineSupport\DoctrineSupportServiceProvider;
+use Larapack\VoyagerHooks\VoyagerHooksServiceProvider;
+use TCG\Voyager\Events\AlertsCollection;*/
+
+class TheCoreServiceProvider extends SupportServiceProvider
+{
+    public function boot()
+    {
+       //dd( 'It works!');
+    }
+
+    public function register()
+    {
+
+    }
+}
