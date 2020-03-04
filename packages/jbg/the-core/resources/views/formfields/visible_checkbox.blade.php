b08a25473eca402d1dd94bf7e5a3c36455ca3df1
Chee <tobyl19@hotmail.com>
2020-03-04 17:19:52 +0900

    working on visible button

--- packages/jbg/the-core/resources/views/formfields/visible_checkbox.blade.php
+++ packages/jbg/the-core/resources/views/formfields/visible_checkbox.blade.php
@@ -1,5 +1,4 @@
-<br>
-<i class="voyager-angle-up"></i>
+<link rel="stylesheet" href="{{ voyager_asset('css/checkbox.min.css') }}">
 <?php $checked = false; ?>
 @if(isset($dataTypeContent->{$row->field}) || old($row->field))
     <?php $checked = old($row->field, $dataTypeContent->{$row->field}); ?>
@@ -7,14 +6,21 @@
     <?php $checked = isset($options->checked) &&
         filter_var($options->checked, FILTER_VALIDATE_BOOLEAN) ? true: false; ?>
 @endif
+<div class="visible_chk_panel">
+    <br>
+    <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+    <label class="el-switch el-switch-sm">
+    @if(isset($options->on) && isset($options->off))
+        <input type="checkbox" name="{{ $row->field }}"
+            data-on="{{ $options->on }}" {!! $checked ? 'checked="checked"' : '' !!}
+            data-off="{{ $options->off }}">
+    @else
+        <input type="checkbox" name="{{ $row->field }}"
+            @if($checked) checked @endif>
+    @endif
+      <span class="el-switch-style"></span>
+    </label>
+    <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+</div>
 
-<?php $class = $options->class ?? "toggleswitch"; ?>
 
-@if(isset($options->on) && isset($options->off))
-    <input type="checkbox" name="{{ $row->field }}" class="{{ $class }}"
-        data-on="{{ $options->on }}" {!! $checked ? 'checked="checked"' : '' !!}
-        data-off="{{ $options->off }}">
-@else
-    <input type="checkbox" name="{{ $row->field }}" class="{{ $class }}"
-        @if($checked) checked @endif>
-@endif
