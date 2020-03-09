198c1a50cb9e458f75b3428239d91000144667c0
Chee <tobyl19@hotmail.com>
2020-03-09 11:56:45 +0900

    working on data table

--- packages/jbg/the-core/resources/views/formfields/sub_section_form.blade.php
+++ packages/jbg/the-core/resources/views/formfields/sub_section_form.blade.php
@@ -2,27 +2,47 @@
         placeholder="{{ old($row->field, $options->placeholder ?? $row->getTranslatedAttribute('display_name')) }}"
        {!! isBreadSlugAutoGenerator($options) !!}
        value="{{ old($row->field, $dataTypeContent->{$row->field} ?? $options->default ?? '') }}"-->
-
+@php
+    $dataSourceJson = old($row->field, $dataTypeContent->{$row->field} ?? $options->default ?? '');
+    if (empty($dataSourceJson))
+        $dataSourceJson = '[]';
+    $dataSource = json_decode($dataSourceJson);
+    var_dump($dataSource);
+     var_dump($row);
+@endphp
 <div class="subsection_dtable_panel">
 <table id="subsection_dtable_{{ $row->field }}" class="display">
     <thead>
         <tr>
-            <th></th>
-            <th>Position</th>
-            <th>Page Title</th>
+        @foreach($row->details->data_table->columns as $columnInfo)
+            <th>{{ $columnInfo->title }}</th>
+        @endforeach
         </tr>
     </thead>
     <tbody>
+        @foreach($dataSource as $rowInfo)
         <tr>
-            <td>UP DOWN</td>
-            <td>1</td>
-            <td>ABC restaurant</td>
+            @foreach($row->details->data_table->columns as $columnInfo)
+                @php
+                    $displayValue = '';
+                    $field = $columnInfo->field;
+                    if (property_exists($rowInfo, $field)) {
+                        $displayValue = $rowInfo->$field;
+                    }
+                @endphp
+                @if ($columnInfo->input_type == 'UpDown')
+                    <td>
+                        <button type="button" class="btn btn-xs btn-primary btn-pos" data-pos="up" data-id="{{ $rowInfo->id }}"><i class="voyager-angle-up"></i></button>
+                        <button type="button" class="btn btn-xs btn-primary btn-pos" data-pos="down" data-id="{{ $rowInfo->id }}"><i class="voyager-angle-down"></i></button>
+                    </td>
+                @elseif ($columnInfo->input_type == 'Text')
+                    <td><input style="width: 100%" type="text" id="text-{{ $rowInfo->id }}" data-id="{{ $rowInfo->id }}" value="{{ $displayValue }}"/></td>
+                @else
+                    <td>{{ $displayValue }}</td>
+                @endif
+            @endforeach
         </tr>
-        <tr>
-            <td>UP DOWN</td>
-            <td>2</td>
-            <td>BAR test</td>
-        </tr>
+        @endforeach
     </tbody>
 </table>
 </div>
