ea90d798d748694cf6ec87b81094eac2e146cefc
Chee <tobyl19@hotmail.com>
2020-03-09 13:51:56 +0900

    working on data table

--- packages/jbg/the-core/resources/views/formfields/sub_section_form.blade.php
+++ packages/jbg/the-core/resources/views/formfields/sub_section_form.blade.php
@@ -15,11 +15,12 @@
        { title: "{{ $columnInfo->title }}", data: "{{ $columnInfo->field }}"
             @if ($columnInfo->input_type == 'UpDown')
                , render: function (data, type, row) {
-                   return 'up down';
+                   return '<button type="button" class="btn btn-xs btn-primary btn-pos" data-pos="up" data-id="' + row.id +'"><i class="voyager-angle-up"></i></button>' +
+                    '&nbsp;<button type="button" class="btn btn-xs btn-primary btn-pos" data-pos="down" data-id="' + row.id +'"><i class="voyager-angle-down"></i></button>';
                }
             @elseif ($columnInfo->input_type == 'Text')
                 , render: function (data, type, row) {
-                    return '<input style="width: 100%" type="text" id="text-'  + row.id + '" data-id="' + row.id +'" value="' + row.{{ $columnInfo->field }} + '"/>';
+                    return '<span style="display: none">' + row.{{ $columnInfo->field }} + '</span><input style="width: 100%" type="text" id="text-'  + row.id + '" data-id="' + row.id +'" value="' + row.{{ $columnInfo->field }} + '"/>';
                 }
             @endif
        },
