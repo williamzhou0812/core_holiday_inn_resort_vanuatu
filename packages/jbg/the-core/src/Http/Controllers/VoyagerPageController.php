7d97fe3244bd7bba64571e1263c05109b2c5e509
Chee <tobyl19@hotmail.com>
2020-03-14 12:06:36 +0900

    Load read page

--- packages/jbg/the-core/src/Http/Controllers/VoyagerPageController.php
+++ packages/jbg/the-core/src/Http/Controllers/VoyagerPageController.php
@@ -208,4 +208,74 @@ class VoyagerPageController extends VoyagerBaseController
             'sectionDataTypeContent'
         ));
     }
+
+    public function show(Request $request, $id)
+    {
+        // set default $tableReference as first item table reference value
+        $tableReference = '';
+        // check if parameter request exists
+        $tableRequest = $request->query('table');
+        if (isset($tableRequest) && !empty($tableRequest)) {
+            $tableReference = $tableRequest;
+        }
+
+        // retrieve data type
+        $dataType = null;
+        if (isset($tableReference)) {
+            // GET THE DataType based on the name
+            $dataType = Voyager::model('DataType')->where('name', '=', $tableReference)->first();
+        }
+        else {
+            // GET THE SLUG, ex. 'posts', 'pages', etc.
+            $slug = $this->getSlug($request);
+
+            // GET THE DataType based on the slug
+            $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+        }
+
+        // check if data type exists
+        if (!isset($dataType)) {
+            abort(404); // request table not found, show 404 page
+        }
+
+        // ensure slug is provided
+        $slug = $dataType->slug;
+
+        $isSoftDeleted = false;
+
+        if (strlen($dataType->model_name) != 0) {
+            $model = app($dataType->model_name);
+
+            // Use withTrashed() if model uses SoftDeletes and if toggle is selected
+            if ($model && in_array(SoftDeletes::class, class_uses_recursive($model))) {
+                $model = $model->withTrashed();
+            }
+            if ($dataType->scope && $dataType->scope != '' && method_exists($model, 'scope'.ucfirst($dataType->scope))) {
+                $model = $model->{$dataType->scope}();
+            }
+            $dataTypeContent = call_user_func([$model, 'findOrFail'], $id);
+            if ($dataTypeContent->deleted_at) {
+                $isSoftDeleted = true;
+            }
+        } else {
+            // If Model doest exist, get data from table name
+            $dataTypeContent = DB::table($dataType->name)->where('id', $id)->first();
+        }
+
+        // Replace relationships' keys for labels and create READ links if a slug is provided.
+        $dataTypeContent = $this->resolveRelations($dataTypeContent, $dataType, true);
+
+        // If a column has a relationship associated with it, we do not want to show that field
+        $this->removeRelationshipField($dataType, 'read');
+
+        // Check permission
+        $this->authorize('read', $dataTypeContent);
+
+        // Check if BREAD is Translatable
+        $isModelTranslatable = is_bread_translatable($dataTypeContent);
+
+        $view = 'voyager::pages.read';
+
+        return Voyager::view($view, compact('dataType', 'dataTypeContent', 'isModelTranslatable', 'isSoftDeleted'));
+    }
 }
