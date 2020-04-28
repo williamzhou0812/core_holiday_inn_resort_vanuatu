e7787174ae21a6fcc4ee85272c958976367ba746
Chee <tobyl19@hotmail.com>
2020-04-28 15:55:59 +0900

    add datetime filter

--- app/Http/Controllers/ShowcasesController.php
+++ app/Http/Controllers/ShowcasesController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use App\Showcase;
 
 class ShowcasesController extends Controller
 {
     public function showcases() {
-        return response()->json(Showcase::where('display_status','1')->get(), 200);
+
+        return response()->json(Showcase::where('display_status','1')
+            ->where(function($q){
+                // from and to are defined
+                $now = new \DateTime('now');
+                $q->where('display_from', '<=', $now->format('Y-m-d H:i'));
+                $q->where('display_to', '>=',  $now->format('Y-m-d H:i'));
+            })
+            ->orWhere(function($q) {
+                // from is in the past and to not defined
+                $now = new \DateTime('now');
+                $q->where('display_from', '<=', $now->format('Y-m-d H:i'));
+                $q->whereNull('display_to');
+            })
+            ->orWhere(function($q) {
+                // both from and to are NULL
+                $q->whereNull('display_from');
+                $q->whereNull('display_to');
+            })
+            ->orWhere(function($q) {
+                // both from and to are NULL
+                $now = new \DateTime('now');
+                $q->whereNull('display_from');
+                $q->where('display_to', '>=',  $now->format('Y-m-d H:i'));
+            })
+            ->get(), 200);
     }
 }
