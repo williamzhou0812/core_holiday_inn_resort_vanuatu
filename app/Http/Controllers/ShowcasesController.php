70b12819349fd01973de1188f5aa30a25b3ac086
Chee <tobyl19@hotmail.com>
2020-04-28 16:00:19 +0900

    move display status to inner function

--- app/Http/Controllers/ShowcasesController.php
+++ app/Http/Controllers/ShowcasesController.php
@@ -10,27 +10,31 @@ class ShowcasesController extends Controller
 {
     public function showcases() {
 
-        return response()->json(Showcase::where('display_status','1')
-            ->where(function($q){
+        return response()->json(Showcase::
+            where(function($q){
                 // from and to are defined
                 $now = new \DateTime('now');
+                $q->where('display_status','1');
                 $q->where('display_from', '<=', $now->format('Y-m-d H:i'));
                 $q->where('display_to', '>=',  $now->format('Y-m-d H:i'));
             })
             ->orWhere(function($q) {
                 // from is in the past and to not defined
                 $now = new \DateTime('now');
+                $q->where('display_status','1');
                 $q->where('display_from', '<=', $now->format('Y-m-d H:i'));
                 $q->whereNull('display_to');
             })
             ->orWhere(function($q) {
                 // both from and to are NULL
+                $q->where('display_status','1');
                 $q->whereNull('display_from');
                 $q->whereNull('display_to');
             })
             ->orWhere(function($q) {
                 // both from and to are NULL
                 $now = new \DateTime('now');
+                $q->where('display_status','1');
                 $q->whereNull('display_from');
                 $q->where('display_to', '>=',  $now->format('Y-m-d H:i'));
             })
