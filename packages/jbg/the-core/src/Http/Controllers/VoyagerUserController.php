db476e335414d2e6fee4ade0525fe48dbec3e8c8
Chee <tobyl19@hotmail.com>
2020-05-27 18:01:10 +0900

    ensure non admin user cannot edit admin user

--- packages/jbg/the-core/src/Http/Controllers/VoyagerUserController.php
+++ packages/jbg/the-core/src/Http/Controllers/VoyagerUserController.php
@@ -387,4 +387,64 @@ class VoyagerUserController extends VoyagerBaseController
 
         return Voyager::view($view, compact('dataType', 'dataTypeContent', 'isModelTranslatable', 'isSoftDeleted'));
     }
+
+    public function edit(Request $request, $id)
+    {
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        if (strlen($dataType->model_name) != 0) {
+            $model = app($dataType->model_name);
+
+            // Use withTrashed() if model uses SoftDeletes and if toggle is selected
+            if ($model && in_array(SoftDeletes::class, class_uses_recursive($model))) {
+                $model = $model->withTrashed();
+            }
+            if ($dataType->scope && $dataType->scope != '' && method_exists($model, 'scope'.ucfirst($dataType->scope))) {
+                $model = $model->{$dataType->scope}();
+            }
+            $dataTypeContent = call_user_func([$model, 'findOrFail'], $id);
+        } else {
+            // If Model doest exist, get data from table name
+            $dataTypeContent = DB::table($dataType->name)->where('id', $id)->first();
+        }
+
+        foreach ($dataType->editRows as $key => $row) {
+            $dataType->editRows[$key]['col_width'] = isset($row->details->width) ? $row->details->width : 100;
+        }
+
+        // If a column has a relationship associated with it, we do not want to show that field
+        $this->removeRelationshipField($dataType, 'edit');
+
+        // Check permission
+        $this->authorize('edit', $dataTypeContent);
+
+        // get admin roles
+        $adminRoleIds = $this->getAdminRoleIds();
+        // get current user
+        $user = \Auth::user();
+        // check if this user is administrator
+        $userIsAdmin = in_array($user->role_id, $adminRoleIds);
+        if (!$userIsAdmin) {
+            // check if desire user is admin role
+            foreach($adminRoleIds as $roleId) {
+                if ($dataTypeContent->role_id == $roleId) {
+                    // not authorize
+                    abort(404);
+                }
+            }
+        }
+        
+        // Check if BREAD is Translatable
+        $isModelTranslatable = is_bread_translatable($dataTypeContent);
+
+        $view = 'voyager::bread.edit-add';
+
+        if (view()->exists("voyager::$slug.edit-add")) {
+            $view = "voyager::$slug.edit-add";
+        }
+
+        return Voyager::view($view, compact('dataType', 'dataTypeContent', 'isModelTranslatable'));
+    }
 }
